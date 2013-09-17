36d89eb5c4b6e29390a380e6255726a0a2561d35
Kartikey Mishra <kartiknnn@gmail.com>
2013-09-17 12:58:09 +0530

    Correct everything related to device model

--- products/pe_jfltexx.mk
+++ products/pe_jfltexx.mk
@@ -1,17 +1,16 @@
-
  # Check for target product
-ifeq (pe_i9505,$(TARGET_PRODUCT)) 
+ifeq (pe_jfltexx,$(TARGET_PRODUCT)) 
 
 # include PE common configuration
 include vendor/pe/config/pe_common.mk
 
 # Inherit CM device configuration
-$(call inherit-product, device/samsung/i9505/cm.mk)
+$(call inherit-product, device/samsung/jfltexx/cm.mk)
 
 # vendor hack
-$(call vendor-replace,samsung,i9505)
+$(call vendor-replace,samsung,jfltexx)
 
-PRODUCT_NAME := pe_i9505
+PRODUCT_NAME :=pe_jfltexx 
 
 # Update local_manifest.xml
 GET_PROJECT_RMS := $(shell vendor/pac/tools/removeprojects.py $(PRODUCT_NAME))
