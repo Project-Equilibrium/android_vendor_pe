f3aaf0fe9a0e009e3b778b4dabfcc30bb72ec1dd
Kartikey Mishra <kartiknnn@gmail.com>
2013-09-17 16:07:06 +0530

    Corrected the way Adam said

--- products/pe_jfltexx.mk
+++ products/pe_jfltexx.mk
@@ -10,7 +10,7 @@ $(call inherit-product, device/samsung/jfltexx/cm.mk)
 # vendor hack
 $(call vendor-replace,samsung,jfltexx)
 
-PRODUCT_NAME :=pe_jfltexx 
+PRODUCT_NAME := pe_jfltexx 
 
 # Update local_manifest.xml
 GET_PROJECT_RMS := $(shell vendor/pac/tools/removeprojects.py $(PRODUCT_NAME))
