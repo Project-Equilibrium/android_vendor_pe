3cb779afe78a5df451f2fa5c4d3d8a39760f8b05
Adam77Root <adam77root@gmail.com>
2013-09-18 17:40:43 +0200

    Fix mako

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,5 +3,5 @@ add_lunch_combo pe_i9103-userdebug
 add_lunch_combo pe_i9500-userdebug
 add_lunch_combo pe_jfltexx-userdebug
 # LGE
+add_lunch_combo pe_mako-userdebug
 add_lunch_combo pe_p880-userdebug
-add_lunch_combo mako userdebug
