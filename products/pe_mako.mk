d16950d4be802b040034f7996ff992690979d20a
Kartikey Mishra <kartiknnn@gmail.com>
2013-09-18 21:11:34 +0530

    Create pe_mako.mk

---
+++ products/pe_mako.mk
@@ -0,0 +1,16 @@
+# Check for target product
+ifeq (pe_mako,$(TARGET_PRODUCT))
+
+# include PE common configuration
+include vendor/pe/config/pe_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/lge/mako/cm.mk)
+
+PRODUCT_NAME := mako
+
+# Update local_manifest.xml
+GET_PROJECT_RMS := $(shell vendor/pe/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/pe/tools/addprojects.py $(PRODUCT_NAME))
+
+endif
