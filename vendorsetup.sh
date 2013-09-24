e974201fd42629b230d943443c18b15787c9875f
Kartikey Mishra <kartiknnn@gmail.com>
2013-09-24 21:41:43 +0530

    Update vendorsetup.sh
    Add n7100:-P

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,7 @@
 add_lunch_combo pe_i9103-userdebug
 add_lunch_combo pe_i9500-userdebug
 add_lunch_combo pe_jfltexx-userdebug
+add_lunch_combo pe_n7100-userdebug
 # LGE
 add_lunch_combo pe_mako-userdebug
 add_lunch_combo pe_p880-userdebug
